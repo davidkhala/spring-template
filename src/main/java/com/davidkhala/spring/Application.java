c2d9b9f021844158a1330b38d316d423464a6294
David Liu <david-khala@hotmail.com>
2024-03-23 15:22:44 +0800

    add data to example

--- src/main/java/com/davidkhala/spring/Application.java
+++ src/main/java/com/davidkhala/spring/Application.java
@@ -1,9 +1,15 @@
 package com.davidkhala.spring;
 
+import com.davidkhala.spring.data.Customer;
+import com.davidkhala.spring.data.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 @RestController
@@ -16,6 +22,35 @@ public class Application {
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            PrintStream console = System.out;
+            // save a few customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+
+            repository.findAll().forEach(customer -> {
+                console.printf(customer.toString());
+            });
+
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L);
+            console.printf(customer.toString());
+
+            // fetch customers by last name
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                console.printf(bauer.toString());
+            });
+        };
     }
 
 }
