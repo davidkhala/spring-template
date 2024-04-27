37f23bb548a6a919f0f67ddc790349d2c977567b
David Liu <david-khala@hotmail.com>
2024-04-28 07:48:13 +0800

    split tests

---
+++ src/test/java/com/davidkhala/spring/SpringTest.java
@@ -0,0 +1,10 @@
+package com.davidkhala.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringTest {
+    @Test
+    public void sanCheck() {}
+}
--- src/test/java/com/davidkhala/spring/ToolsTest.java
+++ src/test/java/com/davidkhala/spring/ToolsTest.java
@@ -2,10 +2,8 @@ package com.davidkhala.spring;
 
 import davidkhala.common.FileTool;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApplicationTests {
+class ToolsTest {
 
     @Test
     void contextLoads() {
